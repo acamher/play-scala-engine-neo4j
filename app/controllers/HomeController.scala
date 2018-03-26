0b2c5c92a17173d462812a37a9b54840c7aed198
fcammed <fcammed@yahoo.es>
2018-03-26 14:13:40 +0200

    Se añade:  - En el Form un Campo input para especificar códigos a consultar.  - Botón de generación automática de Códigos (35) y lanza automáticamente la consulta.  - Cambiado la regla en routes, para conformar los parámetros GET

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,6 +8,7 @@ import scala.concurrent.ExecutionContext
 import org.neo4j.driver.v1._
 import com.vector._
 import play.api.inject.ApplicationLifecycle
+import scala.util.Random
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -27,16 +28,26 @@ class HomeController @Inject()(cc: ControllerComponents)(implicit exec: Executio
   val db = new Neo4j(lifecycle)
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index(""))
   }
 
-  def getServicePromo(query: String) = Action { implicit request: Request[AnyContent] => {
+  def autogencodigos() = Action { implicit request: Request[AnyContent] =>
+    val codg = 1 to 35 map { _ => "Cod" + Random.nextInt(1000000) }
+    val query2 = codg.mkString(",")
+    val resultado = db.getAsociatepromotions(codg.toList)
+    Ok(views.html.result(resultado._1, resultado._2,query2))
+    //Ok(views.html.index(codg.mkString(",")))
+  }
+
+
+  def getServicePromo = Action { implicit request: Request[AnyContent] => {
     //Logger.info("Enviado tick - " + tipo + " - tick connected")
     //val lista: List[String] = List ("Cod1", "Cod3","Cod8")
-    val lista: List[String] = query.split(",").toList
+    val query2: String = request.getQueryString("input_codes").getOrElse(request.toString())
+    val lista: List[String] = query2.split(",").toList
     val resultado = db.getAsociatepromotions(lista)
     //Ok(views.html.index(resultado._1 + " en " + resultado._2.toString + " milisec"))
-    Ok(views.html.result(resultado._1, resultado._2))
+    Ok(views.html.result(resultado._1, resultado._2,query2))
     }
   }
 
